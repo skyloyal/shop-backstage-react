24ffad02ed0ef52c37b8f89b912a3be465a0c12b
skyloyal <913531825@qq.com>
2021-12-02 16:55:39 +0800

    已完成商品管理-分类参数、商品分类功能

--- src/components/good/cate/index.jsx
+++ src/components/good/cate/index.jsx
@@ -1,10 +1,326 @@
-import React, { Component } from 'react';
+// react
+import React, { Component, createRef } from 'react';
 
+// antd
+import {
+  Button, Modal, Table, Row,
+  Col, Tag, Pagination, Form,
+  Input, Card, Breadcrumb,
+  Cascader
+} from 'antd'
+
+// 自定义scss
+import './index.scss'
 class Index extends Component {
+
+  // history
+  history = this.props.history
+  // ref
+  addCateDialogRef = createRef()
+  editCateDialogRef = createRef()
+  // 数据渲染
+  state = {
+    // 商品分类列表
+    cateList: [],
+    // 父级分类列表
+    parentCateList: [],
+    // 级联选择器选中的父级分类列表
+    selectedCateIdArr: [],
+    // 商品分类table的展示列
+    columns: [
+      {
+        title: '商品分类名称',
+        key: 'cat_name',
+        dataIndex: 'cat_name'
+      },
+      {
+        title: '分类ID',
+        key: 'cat_id',
+        dataIndex: 'cat_id'
+      },
+      {
+        title: '分类等级',
+        key: 'cat_level',
+        dataIndex: 'cat_level',
+        render: (text) => {
+          if (text === 0) {
+            return (<Tag color="#87d068">一级</Tag>)
+          } else if (text === 1) {
+            return (<Tag color="#e9ad52">二级</Tag>)
+          } else {
+            return (<Tag color="#909399">三级</Tag>)
+          }
+        }
+      },
+      {
+        title: '操作',
+        render: (_, record) => {
+          return (
+            <>
+              <Button
+                onClick={this.showEditCateDialog(record)}
+                className="primary actionBtn" size="small">编辑</Button>
+              <Button
+                onClick={this.showRemoveCateDialog(record.cat_id)}
+                className="danger actionBtn" size="small">删除</Button>
+            </>
+          )
+        }
+      }
+    ],
+    // 请求对象
+    queryInfo: {
+      pagenum: 1,
+      pagesize: 5
+    },
+    // 商品分类的总数量
+    total: 0,
+    addCateDialogVisible: false,
+    editCateDialogVisible: false,
+  }
+  // [methods]获取商品分类列表
+  getCateList = async () => {
+    const { data: res } = await this.$axios.get('/categories', {
+      params: this.state.queryInfo
+    })
+    console.log(res)
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取商品列表失败')
+    }
+    this.setState({
+      cateList: res.data.result,
+      total: res.data.total
+    })
+  }
+  // [methods]页码，每页页数改变则请求新的数据
+  pageChange = (pagenum, pagesize) => {
+    console.log('pagenum', pagenum)
+    console.log('pagesize', pagesize)
+    if (pagenum === 0) {
+      pagenum = 1
+    }
+    let queryInfo = { pagenum, pagesize }
+    this.setState({ queryInfo }, () => {
+      this.getCateList()
+    })
+  }
+
+  // [methods]展示添加商品分类对话框
+  showAddCateDialog = () => {
+    this.getParentCateList()
+    this.setState({
+      addCateDialogVisible: true
+    }, () => {
+      console.log(this.state.parentCateList)
+    })
+  }
+  // [methods]获取父级商品分类列表
+  getParentCateList = async () => {
+    const { data: res } = await this.$axios.get('/categories', {
+      params: { type: 2 }
+    })
+    console.log(res)
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取父级商品列表失败')
+    }
+    this.setState({
+      parentCateList: res.data
+    })
+  }
+
+  // [methods]添加商品选择父级分类
+  handleParentCateChange = (value) => {
+    this.setState({ selectedCateIdArr: value })
+  }
+  // [methods]提交添加商品分类对话框
+  submitAddCateDialog = async () => {
+    let { cat_name } = this.addCateDialogRef.current.getFieldsValue()
+    cat_name = cat_name.trim()
+    console.log(cat_name)
+    if (!cat_name) {
+      return this.$message.warning('请输入正确的商品分类名称')
+    }
+    const selectedCateIdArr = this.state.selectedCateIdArr
+    const len = selectedCateIdArr.length
+    const cat_pid = (len > 0) ? selectedCateIdArr[len - 1] : 0
+    console.log(this.addCateDialogRef.current)
+    const cat_level = len
+    console.log('cat_pid:', cat_pid)
+    console.log('cat_name:', cat_name)
+    console.log('cat_level:', cat_level)
+
+    const { data: res } = await this.$axios.post('categories', {
+      cat_pid,
+      cat_name,
+      cat_level
+    })
+    if (res.meta.status !== 201) {
+      return this.$message.error('添加商品分类失败')
+    }
+    this.$message.success('添加商品分类成功')
+    this.addCateDialogRef.current.resetFields()
+    this.closeAddCateDialog()
+    this.getCateList()
+  }
+
+  // [methods]关闭添加商品分类对话框
+  closeAddCateDialog = () => {
+    this.setState({
+      addCateDialogVisible: false,
+      selectedCateIdArr: [],
+      parentCateList: []
+    })
+  }
+  // [methods]展示编辑商品分类对话框
+  showEditCateDialog = (record) => () => {
+    console.log(record)
+
+    this.setState({ editCateDialogVisible: true }, () => {
+      console.log(this.editCateDialogRef.current)
+      this.editCateDialogRef.current.setFieldsValue(record)
+    })
+  }
+  submitEditCateDialog = async () => {
+    let { cat_id, cat_name } = this.editCateDialogRef.current.getFieldsValue()
+    cat_name = cat_name.trim()
+    if (!cat_name) {
+      return this.$message.warning('请输入正确的商品分类名称')
+    }
+    const { data: res } = await this.$axios.put(`/categories/${cat_id}`, {
+      cat_name
+    })
+    if (res.meta.status !== 200) {
+      return this.$message.error('编辑商品分类失败')
+    }
+    this.$message.success('编辑商品分类成功')
+    this.closeEditCateDialog()
+    this.getCateList()
+  }
+  closeEditCateDialog = () => {
+    this.setState({
+      editCateDialogVisible: false,
+    })
+    this.editCateDialogRef.current.resetFields()
+  }
+  // [methods]展示删除商品分类对话框
+  showRemoveCateDialog = (cateId) => () => {
+    Modal.confirm({
+      title: "删除商品分类",
+      content: '该操作将永久删除该数据，是否继续?',
+      okText: "确定",
+      cancelText: "取消",
+      onCancel: () => {
+        this.$message.info('已取消删除商品分类操作')
+      },
+      onOk: async () => {
+        const { data: res } = await this.$axios.delete(`/categories/${cateId}`)
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除商品分类失败')
+        }
+        this.$message.success('删除商品分类成功')
+        this.getCateList()
+      }
+    })
+  }
+
+  // [lifecycle]
+  componentDidMount() {
+    this.getCateList()
+  }
+
+  // [lifecycle]
   render() {
     return (
-      <div>
-
+      <div
+        className="goodCate_container">
+        {/* 面包屑导航区 */}
+        <Breadcrumb separator=">">
+          <Breadcrumb.Item href="/home/welcome">首页</Breadcrumb.Item>
+          <Breadcrumb.Item>商品管理</Breadcrumb.Item>
+          <Breadcrumb.Item>分类参数</Breadcrumb.Item>
+        </Breadcrumb>
+        {/* 卡片视图区 */}
+        <Card className="goodCate_show">
+          {/* 操作区 */}
+          <Row>
+            <Col>
+              <Button
+                onClick={this.showAddCateDialog}
+                className="primary">添加分类</Button>
+            </Col>
+          </Row>
+          {/* 展示表格*/}
+          <Table
+            rowKey={record => record.cat_id}
+            className="goodCate_table"
+            dataSource={this.state.cateList}
+            columns={this.state.columns}
+            pagination={false}
+          >
+          </Table>
+          {/* 分页栏 */}
+          <Pagination
+            total={this.state.total}
+            showSizeChanger
+            showQuickJumper
+            showTotal={total => `总计${total}条数据`}
+            pageSize={this.state.queryInfo.pagesize}
+            pageSizeOptions={[5, 10, 20, 50]}
+            onChange={this.pageChange}
+          ></Pagination>
+        </Card>
+        {/* 添加商品分类对话框 */}
+        <Modal
+          title="添加商品分类"
+          visible={this.state.addCateDialogVisible}
+          okText="确认"
+          cancelText="取消"
+          onCancel={this.closeAddCateDialog}
+          onOk={this.submitAddCateDialog}
+        >
+          <Form
+            labelCol={{ span: 4 }}
+            ref={this.addCateDialogRef}>
+            <Form.Item
+              label="父级分类">
+              <Cascader
+                value={this.state.selectedCateIdArr}
+                onChange={this.handleParentCateChange}
+                changeOnSelect
+                expandTrigger="hover"
+                options={this.state.parentCateList}
+                fieldNames={{ value: 'cat_id', label: 'cat_name', children: 'children' }}
+                allowClear
+                placeholder="请选择父级分类"
+              ></Cascader>
+              <span style={{ fontSize: 10, color: 'red' }}>如不选择分类则默认为一级分类</span>
+            </Form.Item>
+            <Form.Item label="分类名称" name="cat_name">
+              <Input></Input>
+            </Form.Item>
+          </Form>
+        </Modal>
+        {/* 编辑商品分类 */}
+        <Modal
+          title="编辑商品分类"
+          visible={this.state.editCateDialogVisible}
+          okText="确认"
+          cancelText="取消"
+          onCancel={this.closeEditCateDialog}
+          onOk={this.submitEditCateDialog}
+        >
+          <Form
+            labelCol={{ span: 4 }}
+            ref={this.editCateDialogRef}>
+            <Form.Item
+              label="父级分类" name="cat_id">
+              <Input disabled></Input>
+            </Form.Item>
+            <Form.Item label="分类名称" name="cat_name">
+              <Input placeholder="请输入商品分类名称"></Input>
+            </Form.Item>
+          </Form>
+        </Modal>
       </div>
     );
   }
--- src/components/good/param/index.jsx
+++ src/components/good/param/index.jsx
@@ -1,10 +1,461 @@
-import React, { Component } from 'react';
+// react
+import React, { Component, createRef } from 'react';
+
+// antd
+import {
+  Breadcrumb, Card, Table,
+  Alert, Cascader, Tabs,
+  Row, Col, Button, Tag,
+  Input, Modal, Form
+} from 'antd'
+import { CloseCircleOutlined } from '@ant-design/icons'
+
+// 自定义scss
+import './index.scss'
+// antd子组件
+const { TabPane } = Tabs;
 
 class Index extends Component {
+
+  // history
+  history = this.props.history
+  // ref
+  // saveInputRef = createRef()
+  editParamDialogRef = createRef()
+
+  // 动态数据渲染
+  state = {
+    // 商品分类列表
+    cateList: [],
+    selectedCateIdArr: [],
+    manyTableData: [],
+    manyTableColumns: [
+      {
+        title: '#',
+        dataIndex: 'attr_id',
+        render: (_, _2, index) => {
+          return index + 1
+        },
+      },
+      {
+        title: '动态属性名称',
+        key: 'attr_name',
+        dataIndex: 'attr_name',
+      },
+      {
+        title: '操作',
+        key: 'attr_id',
+        render: (text, record, index) => {
+          return (
+            <>
+              <Button
+                onClick={this.showEditParamDialog(record)}
+                className="primary actionBtn">编辑</Button>
+              <Button
+                onClick={this.showRemoveParamDialog(record)}
+                className="danger actionBtn">删除</Button>
+            </>
+          )
+        }
+      }
+    ],
+    manyTableExpand: {
+      expandedRowRender: record => {
+        return (
+          <div key={record.attr_id}>
+            {record.attr_valsArr.map((item, index) => {
+              return (
+                <Tag color="#108ee9" key={index}>
+                  {item} &nbsp;<CloseCircleOutlined onClick={this.removeTag(record, index)} />
+                </Tag>
+              )
+            })}
+            {/* react条件渲染不能在花括号内使用if else */}
+            {/* 可用三目运算符或者 "[条件] && (jsx)"代替 */}
+            {record.inputVisible ? (
+              <Input
+                ref={record.inputRef}
+                type="text"
+                size="small"
+                style={{ width: 100 }}
+                onChange={e => this.handleInputChange(e.target.value, record)}
+                onPressEnter={this.submitInputTag(record)}
+                onBlur={this.submitInputTag(record)}
+              ></Input>
+            ) :
+              <Tag
+                visible={!record.inputVisible}
+                onClick={this.showInputTag(record)}
+                className="site-tag-plus">
+                + New Tag
+              </Tag>
+            }
+          </div>
+        )
+      }
+    },
+    // 静态属性列表
+    onlyTableData: [],
+    onlyTableColumns: [
+      {
+        title: '#',
+        dataIndex: 'attr_id',
+        render: (_, _2, index) => {
+          return index + 1
+        },
+      },
+      {
+        title: '动态属性名称',
+        key: 'attr_name',
+        dataIndex: 'attr_name',
+      },
+      {
+        title: '操作',
+        key: 'attr_id',
+        render: (text, record, index) => {
+          return (
+            <>
+              <Button
+                onClick={this.showEditParamDialog(record)}
+                className="primary actionBtn">编辑</Button>
+              <Button
+                onClick={this.showRemoveParamDialog(record)}
+                className="danger actionBtn">删除</Button>
+            </>
+          )
+        }
+      }
+    ],
+    onlyTableExpand: {
+      expandedRowRender: record => {
+        return (
+          <div key={record.attr_id}>
+            {record.attr_valsArr.map((item, index) => {
+              return (
+                <Tag color="#108ee9" key={index}>
+                  {item} &nbsp;<CloseCircleOutlined onClick={this.removeTag(record, index)} />
+                </Tag>
+              )
+            })}
+            {/* react条件渲染不能在花括号内使用if else */}
+            {/* 可用三目运算符或者 "[条件] && (jsx)"代替 */}
+            {record.inputVisible ? (
+              <Input
+                ref={record.inputRef}
+                type="text"
+                size="small"
+                style={{ width: 100 }}
+                onChange={e => this.handleInputChange(e.target.value, record)}
+                onPressEnter={this.submitInputTag(record)}
+                onBlur={this.submitInputTag(record)}
+              ></Input>
+            ) :
+              <Tag
+                visible={!record.inputVisible}
+                onClick={this.showInputTag(record)}
+                className="site-tag-plus">
+                + New Tag
+              </Tag>
+            }
+          </div>
+        )
+      }
+    },
+    activeKey: "0",
+
+    editParamDialogVisible: false,
+    editParamObject: {},
+    removeParamDialogVisible: false,
+    removeParamObject: {},
+  }
+  // [methods] 获取分类列表
+  getCateList = async () => {
+    console.log('开始获取分类列表')
+    const { data: res } = await this.$axios.get('/categories')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取分类列表失败')
+    }
+    console.log(res.data)
+    this.setState({ cateList: res.data })
+  }
+  // [methods] 选择分类后,获取该分类的动态属性和静态属性
+  handleCateChange = async (value) => {
+    let len = value.length
+    // 未选择分类
+    if (len === 0) {
+      this.setState({ selectedCateIdArr: [], manyTableData: [] })
+      // 没有选择三级分类
+    } else if (len < 3) {
+      this.setState({ selectedCateIdArr: [], manyTableData: [] })
+      this.$message.warning('请选择三级分类')
+      // 正确选择三级分类
+    } else {
+      this.setState({ selectedCateIdArr: value })
+      let cateId = value[len - 1]
+      // 获取商品动态属性保存至state.manyTableData
+      this.getManyTableData(cateId)
+      // 获取商品静态属性保存至state.onlyTableData
+      this.getOnlyTableData(cateId)
+    }
+  }
+  // [methods]获取商品动态属性保存至state.manyTableData
+  getManyTableData = async (cateId) => {
+    // 获取动态属性
+    const { data: res } = await this.$axios.get(`/categories/${cateId}/attributes`, {
+      params: {
+        sel: 'many'
+      }
+    })
+    // 错误提示
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取动态属性失败')
+    }
+    // 动态属性预处理
+    let manyTableData = res.data
+    if (manyTableData.length > 0) {
+      manyTableData.forEach(item => {
+        item.attr_valsArr = item.attr_vals ? item.attr_vals.split(' ') : []
+        item.inputVisible = false
+        item.inputValue = ''
+        item.inputRef = createRef()
+      })
+    }
+    console.log(manyTableData)
+    this.setState({ manyTableData })
+  }
+  // [methods]获取商品静态属性保存至state.onlyTableData
+  getOnlyTableData = async (cateId) => {
+    // 获取静态属性
+    const { data: res2 } = await this.$axios.get(`/categories/${cateId}/attributes`, {
+      params: {
+        sel: 'only'
+      }
+    })
+    // 错误提示
+    if (res2.meta.status !== 200) {
+      return this.$message.error('获取静态属性失败')
+    }
+    let onlyTableData = res2.data
+    if (onlyTableData.length > 0) {
+      onlyTableData.forEach(item => {
+        item.attr_valsArr = item.attr_vals ? item.attr_vals.split(' ') : []
+        item.inputVisible = false
+        item.inputValue = ''
+        item.inputRef = createRef()
+      })
+    }
+    // 保存动态属性和静态属性到state
+    console.log(onlyTableData)
+    this.setState({ onlyTableData })
+  }
+  // [methods]切换页签
+  handleActiveKeyChange = (key) => {
+    this.setState({ activeKey: key })
+  }
+  // [methods]展示商品标签输入框
+  showInputTag = (record) => () => {
+    record.inputVisible = true
+    this.setState({}, () => {
+      record.inputRef.current.focus({ cursor: 'start' })
+    })
+  }
+  // [methods]输入商品标签tag
+  handleInputChange = (value, record) => {
+    record.inputValue = value
+  }
+  // [methods]添加商品标签tag
+  submitInputTag = (record) => () => {
+    console.log(record)
+    let inputContent = record.inputValue
+    if (inputContent.trim().length === 0) {
+      record.inputValue = ''
+      record.inputVisible = false
+      this.setState({})
+      return
+    }
+    record.attr_valsArr.push(record.inputValue)
+    record.inputValue = ''
+    this.saveAttrVals(record)
+    this.setState({})
+    record.inputVisible = false
+  }
+  // [methods]发请求保存商品属性
+  saveAttrVals = async (record) => {
+    console.log('saveAttrVals', record)
+    const { data: res } = await this.$axios.put(`categories/${record.cat_id}/attributes/${record.attr_id}`, {
+      attr_name: record.attr_name,
+      attr_sel: record.attr_sel,
+      attr_vals: record.attr_valsArr.join(' ')
+    })
+    if (res.meta.status !== 200) {
+      record.attr_valsArr.pop()
+      return this.$message.error('修改参数项失败')
+    }
+    this.$message.success('修改参数项成功')
+  }
+  // [methods]移除商品标签tag
+  removeTag = (record, index) => () => {
+    record.attr_valsArr.splice(index, 1)
+    this.saveAttrVals(record)
+    this.setState({})
+  }
+  // [methods]展示编辑属性的对话框
+  showEditParamDialog = (record) => () => {
+    console.log(record)
+    this.setState({
+      editParamDialogVisible: true,
+      editParamObject: record
+    }, () => {
+      this.editParamDialogRef.current.setFieldsValue(record)
+      console.log(this.editParamDialogRef.current)
+    })
+
+  }
+  // [methods]提交编辑属性的对话框
+  submitEditParamDialog = async () => {
+    const editParamDialogObject = this.editParamDialogRef.current.getFieldsValue()
+    // console.log(editParamObj)
+    const { attr_name } = editParamDialogObject
+    const { cat_id, attr_id, attr_sel, attr_vals } = this.state.editParamObject
+    const { data: res } = await this.$axios.put(`/categories/${cat_id}/attributes/${attr_id}`, {
+      attr_name,
+      attr_sel,
+      attr_vals
+    })
+    if (res.meta.status !== 200) {
+      return this.$message.error('提交编辑属性失败')
+    }
+    this.$message.success('提交编辑属性成功')
+    if (attr_sel === "many") {
+      this.getManyTableData(cat_id)
+    } else {
+      this.getOnlyTableData(cat_id)
+    }
+    this.closeEditParamDialog()
+  }
+  // [methods]关闭编辑属性的对话框
+  closeEditParamDialog = () => {
+    this.setState({
+      editParamDialogVisible: false,
+      editParamObject: {}
+    }, () => {
+      // console.log(this.editParamDialogRef.current)
+      this.editParamDialogRef.current.resetFields()
+    })
+  }
+  // [methods]展示编辑属性的对话框
+  showRemoveParamDialog = (record) => () => {
+    const { cat_id, attr_id, attr_sel } = record
+    Modal.confirm({
+      title: '删除商品属性',
+      content: '此操作将永久删除该商品属性，是否继续?',
+      okText: "确定",
+      cancelText: "取消",
+      onOk: async () => {
+        const { data: res } = await this.$axios.delete(`/categories/${cat_id}/attributes/${attr_id}`)
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除商品属性失败')
+        }
+        this.$message.success('删除商品属性成功')
+        if (attr_sel === 'many') {
+          this.getManyTableData(cat_id)
+        } else {
+          this.getOnlyTableData(cat_id)
+        }
+      },
+      onCancel: () => {
+        this.$message.info('已取消删除商品属性操作')
+      }
+    })
+  }
+
+  // [lifecycle]
+  componentDidMount() {
+    this.getCateList()
+  }
+  // [lifecycle]
   render() {
     return (
-      <div>
+      <div className="goodParam_container">
+        {/* 面包屑导航区 */}
+        <Breadcrumb separator=">">
+          <Breadcrumb.Item href="/home/welcome">首页</Breadcrumb.Item>
+          <Breadcrumb.Item>商品管理</Breadcrumb.Item>
+          <Breadcrumb.Item>分类参数</Breadcrumb.Item>
+        </Breadcrumb>
+        {/* 卡片视图区 */}
+        <Card className="goodParam_show">
+          <Alert className="goodParam_alert"
+            type="warning" message="注意：只允许为第三级分类设置相关参数！" showIcon
+          ></Alert>
+          <Row>
+            <Col>
+              <span>选择商品分类：</span>
+              <Cascader
+                value={this.state.selectedCateIdArr}
+                className="goodParam_cascader"
+                placeholder="请选择商品分类"
+                allowClear
+                options={this.state.cateList}
+                onChange={this.handleCateChange}
+                fieldNames={{ label: 'cat_name', value: 'cat_id', children: 'children' }}
+              >
+              </Cascader>
+            </Col>
+          </Row>
 
+          <Tabs
+            activeKey={this.state.activeKey}
+            onChange={this.handleActiveKeyChange}
+            tabPosition="top"
+          >
+            <TabPane
+              tab="动态属性"
+              key="0"
+            >
+              <Table
+                expandable={this.state.manyTableExpand}
+                rowKey={record => record.attr_id}
+                dataSource={this.state.manyTableData}
+                columns={this.state.manyTableColumns}
+              >
+              </Table>
+            </TabPane>
+            <TabPane
+              tab="静态属性"
+              key="1"
+            >
+              <Table
+                expandable={this.state.onlyTableExpand}
+                rowKey={record => record.attr_id}
+                dataSource={this.state.onlyTableData}
+                columns={this.state.onlyTableColumns}
+              ></Table>
+            </TabPane>
+          </Tabs>
+        </Card>
+        <Modal
+          visible={this.state.editParamDialogVisible}
+          title="编辑属性"
+          okText="确定"
+          cancelText="取消"
+          onOk={this.submitEditParamDialog}
+          onCancel={this.closeEditParamDialog}
+        >
+          <Form
+            ref={this.editParamDialogRef}
+            labelCol={{ span: 4 }}>
+            <Form.Item name="cat_id" label="分类id">
+              <Input disabled></Input>
+            </Form.Item>
+            <Form.Item name="attr_id" label="属性id">
+              <Input disabled></Input>
+            </Form.Item>
+            <Form.Item name="attr_name" label="属性名称">
+              <Input></Input>
+            </Form.Item>
+          </Form>
+        </Modal>
       </div>
     );
   }
