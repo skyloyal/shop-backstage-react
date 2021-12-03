6b725feda5091bc903caf31e79a2b31efc40926e
skyloyal <913531825@qq.com>
2021-12-03 11:09:40 +0800

    初始化数据统计功能

---
+++ src/components/report/index.jsx
@@ -0,0 +1,16 @@
+// react
+import React, { Component } from 'react';
+
+// 自定义scss
+import './index.scss'
+class Index extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Index;
